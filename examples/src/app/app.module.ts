da9052fc38a407e1e3551446e734b2c7e2193f99
David Stanich <dstanich@gmail.com>
2018-05-09 05:48:05 -0500

    delete components

--- examples/src/app/app.module.ts
+++ examples/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { LeftComponent } from './body/left/left.component';
-import { RightComponent } from './body/right/right.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    LeftComponent,
-    RightComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
