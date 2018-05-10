4a3d3b5510c53faf5c4389b27e20a30fc8d19388
David Stanich <dstanich@gmail.com>
2018-05-09 21:08:03 -0500

    branch steps

--- examples/src/app/app.module.ts
+++ examples/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,7 +14,7 @@ import { GroceryItemComponent } from './grocery-list/grocery-item/grocery-item.c
     GroceryListComponent,
     GroceryItemComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
